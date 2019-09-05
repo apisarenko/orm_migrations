ff95029b9dcc628f3450c2a56883deda2564df57
Andrey <vasily.martynov@gmail.com>
2019-09-05 19:25:05 +0300

    Initial commit

---
+++ school/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from school.views import students_list
+import website
+
+
+urlpatterns = [
+    path('', students_list, name='students'),
+]
+
+if  website.settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
